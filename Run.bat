e6357e214a50b024f386320a5857d38df66c15fd
Jayasakthi Balaji G <91280556+JayasakthiBalajiG@users.noreply.github.com>
2024-04-02 16:03:11 +0530

    Windows bat file uploaded

---
+++ Run.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Predict using YOLOv8n model
+yolo predict model=yolov8n.pt source='TrafficTwo.MP4'
+
+REM Change directory to 'runs'
+cd runs
+
+REM Execute VideoToImages.py script
+python VideoToImages.py
+
+REM Change directory to 'Pics'
+cd Pics
+
+REM Remove .DS_Store files
+for /r %%i in (*.DS_Store) do (
+    del "%%i"
+)
+
+REM Change directory back to the root directory
+cd ..
+
+REM Execute predict.py script
+python predict.py 
